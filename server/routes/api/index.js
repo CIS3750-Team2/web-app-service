3d3e58cbde2916884e0c6ba613605559b9e44c5c
jasonellul <jason.j.ellul@gmail.com>
2019-03-22 14:35:18 -0400

    added route

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -7,4 +7,5 @@ const apiUrl = process.env.API_SERVICE
 module.exports = (app) => {
     app.get('/api/list', require('./list')(apiUrl));
     app.get('/api/count', require('./count')(apiUrl));
+    app.get('/api/export', require('./export')(apiUrl));
 };

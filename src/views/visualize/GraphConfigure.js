55503514f1ce33ca46b706dce16a0734e9f6a880
EvilKanoa <kanoa@kanoa.ca>
2019-03-25 12:13:36 -0400

    Added graph configure controls and made styling cooler for graph containers

--- src/views/visualize/GraphConfigure.js
+++ src/views/visualize/GraphConfigure.js
@@ -4,11 +4,12 @@ import DataFilter from 'widgets/DataFilter';
 import GraphConfigureControls from './GraphConfigureControls'
 import {Divider} from 'antd';
 
-const GraphConfigure = ({ data, onChange }) => (
+const GraphConfigure = ({ data, onChange, wrappedComponentRef }) => (
     <>
         <GraphConfigureControls
             onChange={onChange}
             data={data}
+            wrappedComponentRef={wrappedComponentRef}
         />
         <Divider orientation='left'>
             Filter
